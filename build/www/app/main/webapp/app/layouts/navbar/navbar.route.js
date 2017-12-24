30ad7aec38367b875329ce8c722dfe81704810c3
Costin <costinaldea@yahoo.co.uk>
2017-12-24 15:53:32 +0200

    removing www

--- build/www/app/main/webapp/app/layouts/navbar/navbar.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var navbar_component_1 = require("./navbar.component");
-exports.navbarRoute = {
-    path: '',
-    component: navbar_component_1.NavbarComponent,
-    outlet: 'navbar'
-};
-//# sourceMappingURL=navbar.route.js.map
